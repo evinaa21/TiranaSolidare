fb94fb0a74685e24ab53c49119ca54b8cde35110
uendiall <uendiallushi12@gmail.com>
2026-02-24 20:38:58 +0100

    update homepage design

--- public/index.php
+++ public/index.php
@@ -15,12 +15,9 @@
   <section id="main">
     <div id="main-content">
       <h1>Bashkohu me komunitetin<br> që ndryshon jetë</h1>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis, sit!</p>
+      <p>Së bashku mund të bëjmë më shumë. Ndihmo dikë sot dhe bëhu ndryshimi që dëshiron të shohësh.</p>
       <br><br>
-      <div id="main-links">
-        <a href="/sign-in" class="btn_primary">Bëhu Vullnetar</a>
-        <a href="/ndihmat" class="btn_secondary">Kërko ndihmë <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-move-right-icon lucide-move-right"><path d="M18 8L22 12L18 16"/><path d="M2 12H22"/></svg></a>
-      </div>
+    
       <div id="main-stats">
         <span>
           <b>2000+</b>
@@ -37,43 +34,9 @@
       </div>
     </div>
     <div id="main-help">
-      <div class="help_card">
-        <img src="https://picsum.photos/500/500/">
-        <span class="help_card_status request">Kërkoj ndihmë</span>
-        <span class="help_card_title">Ndihmë me ushqim për familje me 4 anëtarë</span>
-        
-        <div class="help_card_info">
-          <div class="help_card_info_location">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-map-pin-icon lucide-map-pin"><path d="M20 10c0 4.993-5.539 10.193-7.399 11.799a1 1 0 0 1-1.202 0C9.539 20.193 4 14.993 4 10a8 8 0 0 1 16 0"/><circle cx="12" cy="10" r="3"/></svg>
-            Laprakë
-          </div>
-          <div class="help_card_info_time">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-clock-icon lucide-clock"><circle cx="12" cy="12" r="10"/><path d="M12 6v6l4 2"/></svg>
-            2 orë më parë
-          </div>
-        </div>
-        <p class="help_card_description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore repellat consequuntur quam doloribus. Nulla tenetur eveniet ab nostrum consectetur voluptates.</p>
-        <a href="" class="help_card_btn btn_secondary">Ndihmo <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-right-icon lucide-chevron-right"><path d="m9 18 6-6-6-6"/></svg></a>
-      </div>
-      <div class="help_card">
-        <img src="https://picsum.photos/500/501/">
-        <span class="help_card_status request">Kërkoj ndihmë</span>
-        <span class="help_card_title">Ndihmë me ushqim për familje me 4 anëtarë</span>
-        
-        <div class="help_card_info">
-          <div class="help_card_info_location">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-map-pin-icon lucide-map-pin"><path d="M20 10c0 4.993-5.539 10.193-7.399 11.799a1 1 0 0 1-1.202 0C9.539 20.193 4 14.993 4 10a8 8 0 0 1 16 0"/><circle cx="12" cy="10" r="3"/></svg>
-            Laprakë
-          </div>
-          <div class="help_card_info_time">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-clock-icon lucide-clock"><circle cx="12" cy="12" r="10"/><path d="M12 6v6l4 2"/></svg>
-            2 orë më parë
-          </div>
-        </div>
-        <p class="help_card_description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore repellat consequuntur quam doloribus. Nulla tenetur eveniet ab nostrum consectetur voluptates.</p>
-        <a href="" class="help_card_btn btn_secondary">Ndihmo <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-right-icon lucide-chevron-right"><path d="m9 18 6-6-6-6"/></svg></a>
-      </div>
-    </div>
+  <img src="assets/images/logo.png" class="hero-img">
+</div>
+    
   </section>
 
   <section id="si-funksionon">
@@ -84,27 +47,27 @@
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-users-round-icon lucide-users-round"><path d="M18 21a8 8 0 0 0-16 0"/><circle cx="10" cy="8" r="5"/><path d="M22 20c0-3.37-2-6.5-4-8a5 5 0 0 0-.45-8.3"/></svg>
         <span class="card_number">1</span>
         <h3>Kërko ndihmë ose bëhu vullnetar</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.</p>
+        <p>Krijo një llogari në platformën tonë.</p>
       </div>
       <div class="card">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-binoculars-icon lucide-binoculars"><path d="M10 10h4"/><path d="M19 7V4a1 1 0 0 0-1-1h-2a1 1 0 0 0-1 1v3"/><path d="M20 21a2 2 0 0 0 2-2v-3.851c0-1.39-2-2.962-2-4.829V8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v11a2 2 0 0 0 2 2z"/><path d="M 22 16 L 2 16"/><path d="M4 21a2 2 0 0 1-2-2v-3.851c0-1.39 2-2.962 2-4.829V8a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v11a2 2 0 0 1-2 2z"/><path d="M9 7V4a1 1 0 0 0-1-1H6a1 1 0 0 0-1 1v3"/></svg>
         <span class="card_number">2</span>
         <h3>Jep ose merr ndihmë</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.</p>
+        <p>Posto një kërkesë për ndihmë ose ofro shërbimin tënd.</p>
       </div>
 
       <div class="card">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-hand-helping-icon lucide-hand-helping"><path d="M11 12h2a2 2 0 1 0 0-4h-3c-.6 0-1.1.2-1.4.6L3 14"/><path d="m7 18 1.6-1.4c.3-.4.8-.6 1.4-.6h4c1.1 0 2.1-.4 2.8-1.2l4.6-4.4a2 2 0 0 0-2.75-2.91l-4.2 3.9"/><path d="m2 13 6 6"/></svg>
         <span class="card_number">3</span>
         <h3>Kontribuo në komunitet</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.</p>
+        <p>Apliko për vullnetarizëm ose posto kërkesa për ndihmë.</p>
       </div>
 
       <div class="card">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-heart-handshake-icon lucide-heart-handshake"><path d="M19.414 14.414C21 12.828 22 11.5 22 9.5a5.5 5.5 0 0 0-9.591-3.676.6.6 0 0 1-.818.001A5.5 5.5 0 0 0 2 9.5c0 2.3 1.5 4 3 5.5l5.535 5.362a2 2 0 0 0 2.879.052 2.12 2.12 0 0 0-.004-3 2.124 2.124 0 1 0 3-3 2.124 2.124 0 0 0 3.004 0 2 2 0 0 0 0-2.828l-1.881-1.882a2.41 2.41 0 0 0-3.409 0l-1.71 1.71a2 2 0 0 1-2.828 0 2 2 0 0 1 0-2.828l2.823-2.762"/></svg>
         <span class="card_number">4</span>
         <h3>Ndrysho jetë</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.</p>
+        <p>Shiko ndikimin tënd dhe ndërtoni një komunitet më të fortë.</p>
       </div>
     </div>
   </section>
